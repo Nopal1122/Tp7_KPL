025990620b090dd2d5e27efffc60af266662120c
Naufal <ahmadnaufalalghifari@gmail.com>
2025-04-06 23:48:01 +0700

    menambahkan class datamahasiswa

---
+++ TP7_Kpl/DataMahasiswa103022300006.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.IO;
+using System.Text.Json;
+
+public class DataMahasiswa103022300006
+{
+    public class Nama
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+    }
+
+    public class Mahasiswa
+    {
+        public Nama nama { get; set; }
+        public string nim { get; set; }
+        public string fakultas { get; set; }
+    }
+
+    public static void ReadJSON()
+    {
+        string path = "tp7_1_103022300006.json";
+        string jsonString = File.ReadAllText(path);
+        Mahasiswa mhs = JsonSerializer.Deserialize<Mahasiswa>(jsonString);
+
+        Console.WriteLine($"Nama {mhs.nama.depan} {mhs.nama.belakang} dengan nim {mhs.nim} dari fakultas {mhs.fakultas}");
+    }
+}
