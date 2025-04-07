0418b485bc216c0ddbc50c30f85f86189f5e1b36
imseigen <seigenjane@gmail.com>
2025-04-07 20:04:36 +0700

    Update DataMahasiswa_103022330128.cs

--- TP7_Kpl/DataMahasiswa_103022330128.cs
+++ TP7_Kpl/DataMahasiswa_103022330128.cs
@@ -2,24 +2,24 @@ using System;
 using System.IO;
 using System.Text.Json;
 
-public class Nama  // Class untuk property nama
+public class Nama  
 {
     public string depan { get; set; }
     public string belakang { get; set; }
 }
 
-public class DataMahasiswa_103822339128  // Nama class diperbaiki
+public class DataMahasiswa_103822339128  
 {
-    public Nama nama { get; set; }  // Diubah dari 'Mama' ke 'Nama'
+    public Nama nama { get; set; }  
     public string nim { get; set; }
     public string fakultas { get; set; }
 
     public static void ReadJSON()
     {
         string jsonPath = "tp7_1_103822339128.json";
-        string jsonString = File.ReadAllText(jsonPath);  // Diperbaiki method-nya
-        DataMahasiswa_103822339128 data = JsonSerializer.Deserialize<DataMahasiswa_103822339128>(jsonString);  // Syntax deserialisasi diperbaiki
+        string jsonString = File.ReadAllText(jsonPath);  
+        DataMahasiswa_103822339128 data = JsonSerializer.Deserialize<DataMahasiswa_103822339128>(jsonString);  
 
-        Console.WriteLine($"Nama {data.nama.depan} {data.nama.belakang} dengan nim {data.nim} dari fakultas {data.fakultas}");  // 'depah' diperbaiki
+        Console.WriteLine($"Nama {data.nama.depan} {data.nama.belakang} dengan nim {data.nim} dari fakultas {data.fakultas}");  
     }
 }
